570714977f802d147f168da85c70a49df96e2c37
John Burbridge <johnburbridge@gmail.com>
2011-10-17 00:55:58 -0700

    Adding a little javadoc to test jenkins polling of github

--- applications/example/src/main/java/com/johnburbridge/www/Hello.java
+++ applications/example/src/main/java/com/johnburbridge/www/Hello.java
@@ -2,10 +2,19 @@ package com.johnburbridge.www;
 
 import static com.johnburbridge.common.CONSTANTS.*;
 
+/**
+ * A simple hello pojo
+ * 
+ * @author jburbridge
+ * @since 10/17/2011
+ */
 public class Hello {
 
 	private final String helloMessage = HELLO + " there!";
 	
+	/**
+	 * @return the hello message
+	 */
 	public String getHelloMessage() {
 		return helloMessage;
 	}
