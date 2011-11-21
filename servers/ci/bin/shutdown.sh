30f91f13831bd8a6f2669891009da84071d81f14
John Burbridge <johnburbridge@gmail.com>
2011-11-20 22:53:07 -0800

    Adding startup / shutdown scripts to server instances

---
+++ servers/ci/bin/shutdown.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export CATALINA_HOME=../../../install/apache-tomcat-7.0.12
+export CATALINA_BASE=..
+
+$CATALINA_HOME/bin/shutdown.sh
+
+
---
+++ servers/www/bin/setenv.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+export CATALINA_OPTS="-Xms256m -Xmx512m -XX:MaxPermSize=128m"
