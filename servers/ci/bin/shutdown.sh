c1b5794bd87196a0f967dbc99c5add21f576ca6c
John Burbridge <johnburbridge@gmail.com>
2011-11-22 03:01:19 -0800

    Fixing broken path in startup / shutdown scripts

--- servers/ci/bin/shutdown.sh
+++ servers/ci/bin/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export CATALINA_HOME=../../../install/apache-tomcat-7.0.12
+export CATALINA_HOME=../../../install/apache-tomcat-7.0.22
 export CATALINA_BASE=..
 
 $CATALINA_HOME/bin/shutdown.sh
