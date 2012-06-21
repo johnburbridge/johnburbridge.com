1e64e7375102b0b7958d65a0342fd78f320e8048
John Burbridge <johnburbridge@gmail.com>
2012-06-20 22:13:38 -0700

    retiring www tomcat instance

--- servers/www/bin/setenv.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export CATALINA_OPTS="-Xms256m -Xmx512m -XX:MaxPermSize=128m"
--- servers/www/bin/shutdown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export CATALINA_HOME=../../../install/apache-tomcat-7.0.22
-export CATALINA_BASE=..
-
-$CATALINA_HOME/bin/shutdown.sh
-
-
